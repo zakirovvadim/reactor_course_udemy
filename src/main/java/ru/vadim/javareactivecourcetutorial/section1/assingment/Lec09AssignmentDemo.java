9acfe34bf1b99cda8bb125aa443734d5d11401be
vadimzakirov <vadimzakiroff@gmail.com>
2024-04-28 19:43:12 +0500

    first 9-10 lessons. Little task

---
+++ src/main/java/ru/vadim/javareactivecourcetutorial/section1/assingment/Lec09AssignmentDemo.java
@@ -0,0 +1,16 @@
+package ru.vadim.javareactivecourcetutorial.section1.assingment;
+
+import static ru.vadim.javareactivecourcetutorial.courceUtil.Util.*;
+
+public class Lec09AssignmentDemo {
+    public static void main(String[] args) {
+        FileService.read("file03.txt")
+                .subscribe(onNext(), onError(), onComplete());
+
+//        FileService.write("file03.txt", "This is file 3")
+//                .subscribe(onNext(), onError(), onComplete());
+
+        FileService.delete("file03.txt")
+                .subscribe(onNext(), onError(), onComplete());
+    }
+}
