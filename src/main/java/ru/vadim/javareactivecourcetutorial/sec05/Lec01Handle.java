810f8f1adc2678999e28f108371a71d16732ab95
Vadim <vadim.zakiroff@gmail.com>
2024-08-01 22:53:59 +0500

    Section 05 start

---
+++ src/main/java/ru/vadim/javareactivecourcetutorial/sec05/Lec01Handle.java
@@ -0,0 +1,35 @@
+package ru.vadim.javareactivecourcetutorial.sec05;
+
+import reactor.core.publisher.Flux;
+import ru.vadim.javareactivecourcetutorial.courceUtil.Util;
+/*
+Иногда нам нужно использовать какую то логику, модицируют данные и могут фильтровать, т.е. и мапа и фильтер в одном.
+Для этого можно использовать хендел. Таких модификации как oopsRange можно создавать много - они легковесны.
+ */
+public class Lec01Handle {
+    public static void main(String[] args) {
+        Flux<Integer> range = Flux.range(2, 10);
+        Flux<Object> oopsRange = range.handle((item, sink) -> { // можем модифицировать флакс и выделить в отдельную переменную,и вызывать отдельно
+            if (Integer.valueOf(item) % 2 == 0) {
+                sink.next(item);
+            } else {
+                sink.error(new RuntimeException("oops"));
+            }
+        });
+
+//        oopsRange.subscribe(Util.subscriber());
+
+        Flux.range(0, 10)
+                .handle((item, sink) -> {
+                    switch (item) {
+                        case 1 -> sink.next(-2);
+                        case 4 -> {
+                        }
+                        case 7 -> sink.error(new RuntimeException("oops"));
+                        default -> sink.next(item);
+                    }
+                })
+                .cast(Integer.class)
+                .subscribe(Util.subscriber());
+    }
+}
---
+++ src/main/java/ru/vadim/javareactivecourcetutorial/sec05/Lec02HandleAssingment.java
@@ -0,0 +1,25 @@
+package ru.vadim.javareactivecourcetutorial.sec05;
+
+import com.github.javafaker.Faker;
+import reactor.core.publisher.Flux;
+import ru.vadim.javareactivecourcetutorial.courceUtil.Util;
+
+public class Lec02HandleAssingment {
+    public static void main(String[] args) {
+        Flux<String> generateCountry = Flux.<String>generate(sink -> {
+            String name = Faker.instance().country().name();
+            sink.next(name);
+        });
+
+        generateCountry
+                .cast(String.class)
+                .handle((item, sink) -> {
+                    switch (item) {
+                        case "Canada" -> sink.complete();
+                        case "Russian Federation" -> sink.next(item + " failed the war with Ukraine");
+//                        case "Mongolia" -> sink.error(new RuntimeException("MONGOLIA"));
+                        default -> sink.next(item);
+                    }
+                }).subscribe(Util.subscriber());
+    }
+}
