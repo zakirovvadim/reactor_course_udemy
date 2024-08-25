fc13c912db59dfce1415b77b633b459a5b3ee328
VadimZakirov <appolon17@gmail.com>
2024-08-25 12:24:37 +0500

    ConcatWith

---
+++ src/main/java/ru/vadim/javareactivecourcetutorial/sec09/Lec04ConcatError.java
@@ -0,0 +1,46 @@
+package ru.vadim.javareactivecourcetutorial.sec09;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import ru.vadim.javareactivecourcetutorial.courceUtil.Util;
+
+import java.time.Duration;
+/*
+concatDelayError  откладывает возникновение ошибки в конец
+ */
+
+@Slf4j
+public class Lec04ConcatError {
+    public static void main(String[] args) throws InterruptedException {
+        demo2();
+        Thread.sleep(Duration.ofSeconds(10));
+    }
+
+    private static void demo1() {
+        producer1()
+                .concatWith(producer3())
+                .concatWith(producer2())
+                .subscribe(ru.vadim.updatedCource.common.Util.subscriber());
+    }
+
+    private static void demo2() {
+        Flux.concatDelayError(producer1(), producer3(), producer2())
+                .subscribe(ru.vadim.updatedCource.common.Util.subscriber());
+    }
+
+    private static Flux<Integer> producer1() {
+        return Flux.just(1, 2, 3)
+                .doOnSubscribe(s -> log.info("subscribing to producer1"))
+                .delayElements(Duration.ofMillis(10));
+    }
+
+    private static Flux<Integer> producer2() {
+        return Flux.just(51, 52, 53)
+                .doOnSubscribe(s -> log.info("subscribing to producer2"))
+                .delayElements(Duration.ofMillis(10));
+    }
+
+    private static Flux<Integer> producer3() {
+        return Flux.error(new RuntimeException("oops"));
+    }
+}
---
+++ src/main/java/ru/vadim/javareactivecourcetutorial/sec09/lec03ConcatWith.java
@@ -0,0 +1,51 @@
+package ru.vadim.javareactivecourcetutorial.sec09;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import ru.vadim.javareactivecourcetutorial.courceUtil.Util;
+
+import java.time.Duration;
+import java.util.List;
+/*
+.concatWith противоположный аналог .startWith, т.е. выполняется потребление основного продьюсера,а потом уже того что в concatWith
+concatWith - принимает другой продьюсер
+concatWithValues - обычные значения
+ */
+@Slf4j
+public class lec03ConcatWith {
+
+    public static void main(String[] args) throws InterruptedException {
+        demo3();
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    public static void demo1() {
+        producer1()
+                .concatWithValues(-1, 0)
+                .subscribe(Util.subscriber());
+    }
+
+    public static void demo2() {
+        producer1()
+                .concatWith(producer2())
+                .take(4)
+                .subscribe(Util.subscriber());
+    }
+
+    public static void demo3() {
+        Flux.concat(producer1(), producer2())
+                .subscribe(Util.subscriber());
+    }
+
+    private static Flux<Integer> producer1() {
+        return Flux.just(1, 2, 3)
+                .doOnSubscribe(s -> log.info("subscribing to producer1"))
+                .delayElements(Duration.ofMillis(10));
+    }
+
+    private static Flux<Integer> producer2() {
+        return Flux.just(51, 52, 53)
+                .doOnSubscribe(s -> log.info("subscribing to producer2"))
+                .delayElements(Duration.ofMillis(10));
+    }
+}
