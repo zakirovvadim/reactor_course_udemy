5b09c6525c0e568d6ef6a76a7997ca91cfc79ae9
Vadim <vadim.zakiroff@gmail.com>
2024-08-30 22:01:57 +0500

    lec05Merge p2 finish

--- src/main/java/ru/vadim/javareactivecourcetutorial/sec09/Lec05Merge.java
+++ src/main/java/ru/vadim/javareactivecourcetutorial/sec09/Lec05Merge.java
@@ -7,34 +7,47 @@ import ru.vadim.javareactivecourcetutorial.courceUtil.Util;
 import java.time.Duration;
 import java.util.List;
 
+/*
+merge() - позволяет объединять производителей. Потребление в таком случае идет рандомно. Если использовать ограничение на
+потребление в виде оператора take() то потребятся только указанное количество элементов, успевших произвестись каким-либо производителем
+ */
 @Slf4j
 public class Lec05Merge {
     public static void main(String[] args) {
 
+        demo2();
 
         Util.sleepSeconds(3);
     }
 
-    public static void demo2() {
+    private static void demo1() {
+        Flux.merge(producer1(), producer2(), producer3())
+                .take(2)
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo2() {
         producer1()
-                .startWith(List.of(-2, -1, 0))
+                .mergeWith(producer2())
+                .mergeWith(producer3())
+                .take(2)
                 .subscribe(Util.subscriber());
     }
 
     private static Flux<Integer> producer1() {
         return Flux.just(1, 2, 3)
-                .doOnSubscribe(s -> log.info("subscribing to producer1"))
+                .transform(ru.vadim.updatedCource.common.Util.fluxLogger("producer3"))
                 .delayElements(Duration.ofMillis(10));
     }
 
     private static Flux<Integer> producer2() {
         return Flux.just(51, 52, 53)
-                .doOnSubscribe(s -> log.info("subscribing to producer2"))
+                .transform(ru.vadim.updatedCource.common.Util.fluxLogger("producer3"))
                 .delayElements(Duration.ofMillis(10));
     }
     private static Flux<Integer> producer3() {
         return Flux.just(11, 22, 33)
-                .doOnSubscribe(s -> log.info("subscribing to producer2"))
+                .transform(ru.vadim.updatedCource.common.Util.fluxLogger("producer3"))
                 .delayElements(Duration.ofMillis(10));
     }
 }
--- src/main/java/ru/vadim/updatedCource/common/Util.java
+++ src/main/java/ru/vadim/updatedCource/common/Util.java
@@ -1,9 +1,14 @@
 package ru.vadim.updatedCource.common;
 
 import com.github.javafaker.Faker;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscriber;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.UnaryOperator;
+
+@Slf4j
 public class Util {
 
     public static final Faker faker = Faker.instance();
@@ -18,5 +23,12 @@ public class Util {
 
     public static Faker faker() {
         return faker;
+    }
+
+    public static <T> UnaryOperator<Flux<T>> fluxLogger(String name) {
+        return flux -> flux
+                .doOnSubscribe(s -> log.info("subscribing to {}", name))
+                .doOnCancel(() -> log.info("cancelling {}", name))
+                .doOnComplete(() -> log.info("{} completed", name));
     }
 }
