ffe4e489d12404fab614a65f07ca663603ff81f7
Vadim <vadim.zakiroff@gmail.com>
2024-10-19 04:07:47 +0500

    Add some classes

---
+++ src/main/java/ru/vadim/javareactivecourcetutorial/sec12/assingment/SlackMember.java
@@ -0,0 +1,26 @@
+package ru.vadim.javareactivecourcetutorial.sec12.assingment;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Data
+public class SlackMember {
+
+    private final String name;
+    private Consumer<String> messageConsumer;
+
+    public SlackMember(String name) {
+        this.name = name;
+    }
+
+    public void says(String message) {
+        this.messageConsumer.accept(message);
+    }
+
+    public void receive(String message) {
+        log.info(message);
+    }
+}
