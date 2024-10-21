c3c46acb12d46e7a2db2b10dd9c9d1ddf69431b2
Vadim <vadim.zakiroff@gmail.com>
2024-10-21 23:39:44 +0500

    Lec01MonoTest

---
+++ src/test/java/ru/vadim/javareactivecourcetutorial/Lec01MonoTest.java
@@ -0,0 +1,23 @@
+package ru.vadim.javareactivecourcetutorial;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class Lec01MonoTest {
+
+    private Mono<String> getProduct(int id) {
+        return Mono.fromSupplier(() -> "product-" + id)
+                .doFirst(() -> log.info("invoked"));
+    }
+
+    @Test
+    public void productTest() {
+        StepVerifier.create(getProduct(1))
+                .expectNext("product-1")
+                .expectComplete()
+                .verify();
+    }
+}
